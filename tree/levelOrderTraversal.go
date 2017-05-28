e39a42b2dcd0d1c3cbee83eb080afcfa9f54a3da
siddheshwar <siddheshwar@Tescos-MacBook-Pro.local>
2017-05-28 09:14:53 +0530

    level order traversal using queue

---
+++ tree/levelOrderTraversal.go
@@ -0,0 +1,27 @@
+package tree
+
+import "fmt"
+
+// Perform Level Order traversal and print content on console
+// Uses queue to push Tree nodes
+func levelOrder(root *Node) {
+	if root == nil {
+		return
+	}
+	queue := New()
+	queue.enqueue(root)
+
+	for queue.isEmpty() != true {
+		n := queue.dequeue()
+		fmt.Printf("%d ", n.val)
+
+		if n.left != nil {
+			queue.enqueue(n.left)
+		}
+
+		if n.right != nil {
+			queue.enqueue(n.right)
+		}
+
+	}
+}
---
+++ tree/queue.go
@@ -0,0 +1,28 @@
+package tree
+
+type queue struct {
+	values []*Node
+}
+
+func New() *queue {
+	queue := &queue{}
+	return queue
+}
+
+func (q *queue) enqueue(node *Node) {
+	q.values = append(q.values, node)
+}
+
+func (q *queue) dequeue() *Node {
+	var val Node
+	if q.isEmpty() {
+		return nil
+	}
+	val = *q.values[0]
+	q.values = q.values[1:]
+	return &val
+}
+
+func (q *queue) isEmpty() bool {
+	return len(q.values) == 0
+}
