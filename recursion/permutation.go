c81465d0f7d1333edc91e05423070460e6531adc
siddheshwar <siddheshwar@Tescos-MacBook-Pro.local>
2017-06-11 09:22:41 +0530

    generate permutation

---
+++ recursion/permutation.go
@@ -0,0 +1,28 @@
+// Given a String containing all DISTINCT characters, find all permuations of it
+// This approach will not work if there are duplicates in the String
+
+package recursion
+
+import "fmt"
+
+func permutation(input string) {
+	if len(input) < 2 {
+		return
+	}
+	output := "" // the permutated result gets stored
+	generatePermutation(output, input)
+}
+
+// Time Complexity : O(N.N!)
+//   Each permutation takes O(N); N is number of character in input string
+//   And there are N! permutations
+func generatePermutation(output, input string) {
+	if len(input) == 0 {
+		fmt.Println("perm: ", output)
+		return
+	}
+
+	for i := 0; i < len(input); i++ {
+		generatePermutation(output+input[i:i+1], input[0:i]+input[i+1:]) // won't work if this modification is done before metod call
+	}
+}
