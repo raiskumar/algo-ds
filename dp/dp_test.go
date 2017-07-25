4cc260307e100120d059b7a63a028dfe94e4b260
siddheshwar <siddheshwar@inmac111ah.global.tesco.org>
2017-07-25 18:29:11 +0530

    implemented maxSubArray method

--- dp/dp_test.go
+++ dp/dp_test.go
@@ -9,3 +9,9 @@ func TestFibonacci(t *testing.T) {
 	n := 5
 	fmt.Printf("fibonacci of %d = %d", n, fibonacci(n))
 }
+
+func TestMaxSumSubarray(t *testing.T) {
+	arr := []int{904, 40, 523, 12, -335, -385, -124, 481, 31}
+	i, j, sum := maxSubArray(arr)
+	fmt.Printf("max Sum between %d,%d = %d", i, j, sum)
+}
--- dp/maxSumSubarray.go
+++ dp/maxSumSubarray.go
@@ -8,6 +8,36 @@ package dp
 // Optimized at the cost of O(N) addition space overhead
 // Array stored sum till ith index; i.e. Sum[i] = Sum of all elements until index i
 // Sum[i,j] = Sum[j] - Sum[i-1]
+func maxSubArray(arr []int) (start, end, result int) {
+	len := len(arr)
+	var sum []int
+
+	// populate sum array
+	sum = append(sum, arr[0])
+	for i := 1; i < len; i++ {
+		sum = append(sum, sum[i-1]+arr[i]) //sum[i] = sum[i-1] + arr[i]
+	}
+
+	sumjk := -1
+	for j := 0; j < len-1; j++ {
+		for k := 1; k < len; k++ {
+
+			if j-1 == -1 {
+				sumjk = sum[k] // sum[j-1] = 0
+			} else {
+				sumjk = sum[k] - sum[j-1] //sum(j,k) = sum(k) - sum(j - 1)
+			}
+
+			// Update result variables
+			if result < sumjk {
+				result = sumjk
+				start = j
+				end = k
+			}
+		}
+	}
+	return start, end, result
+}
 
 // (3) DP
 //
